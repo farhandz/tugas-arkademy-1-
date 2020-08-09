d5f80ceb8671a79d59d498d1d43e3c9fa6889fae
farhandz <ammarjoz09@gmail.com>
2020-08-09 14:22:11 +0700

    nested loop validation

--- nested_loop(Soal3).js
+++ nested_loop(Soal3).js
@@ -1,18 +1,15 @@
-// let row = 5
 
-// for (let i = 1; i < row; i++) {
-//     for (let j = 1; j <= (row - i); j++) {
-//         document.write("&nbsp")
-//     }
-//     for (let k = 1; k <= i; k++) {
-//    tugas arka 1
 let data = ""
-for(let i = 5; i >= 1; i--) {
-   for(let j = 1; j <= i; j++) {
-       data += j
-   }
-
-    data += '\n'
-} 
+const row  = "tay"
+if(typeof row !== "number") {
+    console.log("data bukan number")
+} else{
+    for(let i = 5; i >= 1; i--) {
+       for(let j = 1; j <= i; j++) {
+           data += j
+       }
+        data += '\n'
+    } 
+}
 
 console.log(data)
--- task2(soal2).js
+++ task2(soal2).js
@@ -8,18 +8,20 @@ const rata = totaa / 4
 // const rata = 71
 
 
-if (typeof totaa === 'string' || mtk === null || bind === null || bing === null || ipa === null) {
+if (typeof totaa !== 'number' || mtk === null || bind === null || bing === null || ipa === null) {
     console.log("nilai yang di input tidak boleh kosong dan input harus berupa number");
 } else {
     if (rata >= 90 && rata <= 100) {
         console.log(`rata rata anda ${rata} dengan grade A`)
-    } else if (rata >= 80 && rata < 90) {
+    } else if (rata >= 80 && rata < 89) {
         console.log(`rata rata anda ${rata} dengan grade B`)
-    } else if (rata >= 70 && rata < 80) {
+    } else if (rata >= 70 && rata < 70) {
         console.log(`rata rata anda ${rata} dengan grade C`)
-    } else {
+    } else if (rata >= 70 && rata < 70) {
         console.log(`rata rata anda ${rata} dengan grade D`)
-
+        
+    } else {
+        console.log(`rata rata anda ${rata} dengan grade e`)
     }
 }
 
